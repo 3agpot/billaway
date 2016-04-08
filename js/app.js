88615343366431bd53523c02facc4cdd63dd96c5
Rashid <rashid@leadpops.com>
2016-04-07 22:46:19 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+var app = angular.module("billawayApp", ["ui.router"]);
+
+app.controller("FeaturedOffersController", function() {
+    this.images = [
+        {name: "adidas", ext: "png"},
+        {name: "adidas", ext: "png"},
+        {name: "adidas", ext: "png"},
+        {name: "adidas", ext: "png"},
+        {name: "adidas", ext: "png"},
+        {name: "adidas", ext: "png"},
+        {name: "adidas", ext: "png"},
+        {name: "adidas", ext: "png"},
+        {name: "adidas", ext: "png"},
+        {name: "adidas", ext: "png"},
+        {name: "adidas", ext: "png"},
+        {name: "adidas", ext: "png"},
+    ];
+});
